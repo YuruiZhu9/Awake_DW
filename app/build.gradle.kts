9da51ec9bb56d778c6f64160733081e416a3cee3
unknown <zhuyurui@supcon.com>
2026-09-04 17:40:03 +0800

    chore: 发布 v0.3.0-alpha6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.awakedw.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.3.0-alpha5"
+        versionCode = 11
+        versionName = "0.3.0-alpha6"
     }
 
     compileOptions {
